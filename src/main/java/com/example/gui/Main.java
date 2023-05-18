bc4e78a9f37c08cc2d74888f2316154abe9f720e
Youssef Habil <mickeyhabil@gmail.com>
2023-05-18 04:58:36 +0300

    GUI2

--- src/main/java/com/example/gui/Main.java
+++ src/main/java/com/example/gui/Main.java
@@ -6,12 +6,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Welcome to our fantastic library");
         stage.setScene(scene);
         stage.show();
     }
